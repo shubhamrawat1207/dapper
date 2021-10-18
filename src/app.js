fb3b85ef81aa1ae13049d9b336e4d4ff9a9cb636
Shubham Rawat <rawatshubam269@gmail.com>
2021-10-18 15:48:42 +0530

    modified app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const hbs = require("hbs");
+const port = process.env.PORT || 9000
 const app = express();
 
 app.set("view engine","hbs");
@@ -12,6 +13,6 @@ app.get("/",(req,res)=>{
     res.render("index");
 });
 
-app.listen(9000,()=>{
+app.listen(port,()=>{
     console.log(`Server is running on port 9000`);
 });
